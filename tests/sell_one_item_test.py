789dfd2542e0ede30e38241a2cbe5c520c953d41
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 20:02:31 +0100

    Removed the failing hook up test.

--- tests/sell_one_item_test.py
+++ tests/sell_one_item_test.py
@@ -3,9 +3,6 @@ import pytest
 
 
 class SellOneItemTest(unittest.TestCase):
-    def test_failing_hookup(self):
-        self.assertEqual(1 + 1, 2)
-
     @pytest.mark.skip(reason = "Test list")
     def test_sell_one_item_when_item_is_found(self):
         self.assertEqual(display.text, "£2.00")
