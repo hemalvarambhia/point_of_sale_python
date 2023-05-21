40d040e45a61c176dfc4cfd70695bb8aad96e725
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 21:30:59 +0100

    Added another test to our list.

--- tests/sell_one_item_test.py
+++ tests/sell_one_item_test.py
@@ -13,6 +13,15 @@ class SellOneItemTest(unittest.TestCase):
 
         self.assertEqual(display.text, "£2.00")
 
+    @pytest.mark.skip(reason="Test list")
+    def test_sell_any_one_item_when_item_is_found(self):
+        display = Display()
+        point_of_sale = PointOfSale(display)
+
+        point_of_sale.on_barcode("54321")
+
+        self.assertEqual(display.text, "£0.99")
+
     @pytest.mark.skip(reason = "Test list")
     def test_sell_one_item_when_item_is_not_found(self):
         pass
