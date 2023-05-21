651edcfcd9e9278691d2f8f8e8632672015b22a6
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 22:15:10 +0100

    Made the method static.

--- src/point_of_sale.py
+++ src/point_of_sale.py
@@ -6,6 +6,7 @@ class PointOfSale:
         price = self.__price_of_product_with_barcode(barcode)
         self.display.text = price
 
-    def __price_of_product_with_barcode(self, barcode):
+    @staticmethod
+    def __price_of_product_with_barcode(barcode):
         prices_by_barcode = { '54321': 0.99, '12345': 2.00 }
         return '£' + '%0.2f' % (prices_by_barcode[barcode])
