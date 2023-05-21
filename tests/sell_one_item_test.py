d03afd761d0d108708d9322456b1fc45acd66bc4
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 21:22:24 +0100

    Tests passed

--- tests/sell_one_item_test.py
+++ tests/sell_one_item_test.py
@@ -1,10 +1,16 @@
 import unittest
 import pytest
+from src.display import Display
+from src.point_of_sale import PointOfSale
 
 
 class SellOneItemTest(unittest.TestCase):
-    @pytest.mark.skip(reason = "Test list")
     def test_sell_one_item_when_item_is_found(self):
+        display = Display()
+        point_of_sale = PointOfSale(display)
+
+        point_of_sale.on_barcode("12345")
+
         self.assertEqual(display.text, "£2.00")
 
     @pytest.mark.skip(reason = "Test list")
