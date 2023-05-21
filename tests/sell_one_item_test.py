09f2b8d0a11bc8f3985b3c1b0e55a549f8e74720
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 19:58:19 +0100

    Added tests to the list.

--- tests/sell_one_item_test.py
+++ tests/sell_one_item_test.py
@@ -1,9 +1,18 @@
 import unittest
+import pytest
 
 
 class SellOneItemTest(unittest.TestCase):
     def test_failing_hookup(self):
         self.assertEqual(1 + 1, 2)
+
+    @pytest.mark.skip(reason = "Test list")
+    def test_sell_one_item_when_item_is_found(self):
+        self.assertEqual(display.text, "£2.00")
+
+    @pytest.mark.skip(reason = "Test list")
+    def test_sell_one_item_when_item_is_not_found(self):
+        pass
 
 
 if __name__ == '__main__':
