ab6b595160a556d9d4a51f3d1a08ee419535208b
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 23:34:02 +0100

    Moved the formatting price logic out of the method to ensure the method does one thing.

--- src/point_of_sale.py
+++ src/point_of_sale.py
@@ -3,13 +3,14 @@ class PointOfSale:
         self.display = display
 
     def on_barcode(self, barcode):
-        formatted_price = self.__price_of_product_with_barcode(barcode)
+        price = self.__price_of_product_with_barcode(barcode)
+        formatted_price = PointOfSale.__format_price(price)
         self.display.text = formatted_price
 
     @staticmethod
     def __price_of_product_with_barcode(barcode):
         prices_by_barcode = {"54321": 0.99, "12345": 2.00}
-        return PointOfSale.__format_price(prices_by_barcode[barcode])
+        return prices_by_barcode[barcode]
 
     @staticmethod
     def __format_price(price):
