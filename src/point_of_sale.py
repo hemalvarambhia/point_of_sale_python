02bbcc28e4cbbfc41b032b0fcf89e6551b4430e5
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-21 22:14:03 +0100

    Clarified the name of a method.

--- src/point_of_sale.py
+++ src/point_of_sale.py
@@ -3,9 +3,9 @@ class PointOfSale:
         self.display = display
 
     def on_barcode(self, barcode):
-        price = self.__price_for_barcode(barcode)
+        price = self.__price_of_product_with_barcode(barcode)
         self.display.text = price
 
-    def __price_for_barcode(self, barcode):
+    def __price_of_product_with_barcode(self, barcode):
         prices_by_barcode = { '54321': 0.99, '12345': 2.00 }
         return '£' + '%0.2f' % (prices_by_barcode[barcode])
