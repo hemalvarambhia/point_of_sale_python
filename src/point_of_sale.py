43c165d7d160072f4de77c115c6cc939c5a8fb2b
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-22 22:22:40 +0100

    Replaced method call with direct access to the state variable.

--- src/point_of_sale.py
+++ src/point_of_sale.py
@@ -4,10 +4,6 @@ class PointOfSale:
         self.prices_by_barcode = {"54321": 0.99, "12345": 2.00}
 
     def on_barcode(self, barcode):
-        price = self.__price_of_product_with_barcode(barcode)
+        price = self.prices_by_barcode[barcode]
         formatted_price = self.display.format_price(price)
         self.display.text = formatted_price
-
-
-    def __price_of_product_with_barcode(self, barcode):
-        return self.prices_by_barcode[barcode]
