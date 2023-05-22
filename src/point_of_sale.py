615d719ef1462e0a4bc49f24fcd870e1231e9c7d
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-22 22:21:23 +0100

    Pushed a detail up the callstack and made the private method an instance one.

--- src/point_of_sale.py
+++ src/point_of_sale.py
@@ -1,13 +1,13 @@
 class PointOfSale:
     def __init__(self, display):
         self.display = display
+        self.prices_by_barcode = {"54321": 0.99, "12345": 2.00}
 
     def on_barcode(self, barcode):
         price = self.__price_of_product_with_barcode(barcode)
         formatted_price = self.display.format_price(price)
         self.display.text = formatted_price
 
-    @staticmethod
-    def __price_of_product_with_barcode(barcode):
-        prices_by_barcode = {"54321": 0.99, "12345": 2.00}
-        return prices_by_barcode[barcode]
+
+    def __price_of_product_with_barcode(self, barcode):
+        return self.prices_by_barcode[barcode]
