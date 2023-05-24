d9664b53f9a4c58aa10dc85e73f933cda901773e
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-24 22:20:23 +0100

    Improved the names of some tests.

--- tests/sell_one_item_test.py
+++ tests/sell_one_item_test.py
@@ -5,7 +5,7 @@ from src.point_of_sale import PointOfSale
 
 
 class SellOneItemTest(unittest.TestCase):
-    def test_sell_one_item_when_item_is_found(self):
+    def test_when_item_is_found(self):
         display = Display()
         point_of_sale = PointOfSale(display, {"54321": 0.99, "12345": 2.00})
 
@@ -13,7 +13,7 @@ class SellOneItemTest(unittest.TestCase):
 
         self.assertEqual(display.text, "£2.00", "Expected display to show £2.00 but got " + str(display))
 
-    def test_sell_any_one_item_when_item_is_found(self):
+    def test_when_any_item_is_found(self):
         display = Display()
         point_of_sale = PointOfSale(display, {"54321": 0.99, "12345": 2.00})
 
@@ -21,7 +21,7 @@ class SellOneItemTest(unittest.TestCase):
 
         self.assertEqual(display.text, "£0.99", "Expected display to show £0.99 but got " + str(display))
 
-    def test_sell_one_item_when_item_is_not_found(self):
+    def test_when_item_is_not_found(self):
         display = Display()
         point_of_sale = PointOfSale(display, {"11111": 1.50, "77777": 12.01})
 
@@ -29,7 +29,7 @@ class SellOneItemTest(unittest.TestCase):
 
         self.assertEqual(display.text, "Product with barcode 00000 not found")
 
-    def test_sell_one_item_when_any_item_is_not_found(self):
+    def test_when_any_item_is_not_found(self):
         display = Display()
         point_of_sale = PointOfSale(display, {"54321": 1.50, "09912": 12.01})
 
@@ -37,7 +37,7 @@ class SellOneItemTest(unittest.TestCase):
 
         self.assertEqual(display.text, 'Product with barcode 10011 not found')
 
-    @pytest.mark.skip(reason = "Test list")
+    @pytest.mark.skip(reason='Next test to get passing')
     def test_no_barcode(self):
         display = Display()
         point_of_sale = PointOfSale(display, {"85439": 1.50, "83843": 12.01})
