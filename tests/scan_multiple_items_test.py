87f5861fb7cec21d36c554e97a79ba4752f3ce60
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-25 21:16:43 +0100

    We can scan two items when both are found in the catalogue.

--- tests/scan_multiple_items_test.py
+++ tests/scan_multiple_items_test.py
@@ -1,11 +1,22 @@
 import unittest
 import pytest
+from src.display import Display
+from src.point_of_sale import PointOfSale
 
 
 class ScanMultipleItems(unittest.TestCase):
-    @pytest.mark.skip(reason='Test list')
     def test_scanning_two_items_when_both_are_found(self):
-        pass
+        display = Display()
+        point_of_sale = PointOfSale(display, {'11111': 0.99, '59385': 1.00, '33333': 0.20})
+
+        point_of_sale.on_barcode('11111')
+        self.assertEqual(display.text, "£0.99")
+
+        point_of_sale.on_barcode('59385')
+        self.assertEqual(display.text, "£1.00")
+
+        point_of_sale.on_barcode('33333')
+        self.assertEqual(display.text, "£0.20")
 
     @pytest.mark.skip(reason='Test list')
     def test_scanning_two_items_but_only_one_is_found(self):
