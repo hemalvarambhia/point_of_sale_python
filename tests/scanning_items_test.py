352ec7c374328dc442f110d5e3492e33d3e0fd69
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-25 21:52:05 +0100

    Improved the name of a test and merged a test case with another.

--- tests/scanning_items_test.py
+++ tests/scanning_items_test.py
@@ -3,7 +3,47 @@ from src.display import Display
 from src.point_of_sale import PointOfSale
 
 
-class ScanMultipleItems(unittest.TestCase):
+class ScanningItemsTest(unittest.TestCase):
+    def test_when_item_is_found(self):
+        display = Display()
+        point_of_sale = PointOfSale(display, {"54321": 0.99, "12345": 2.00})
+
+        point_of_sale.on_barcode("12345")
+
+        self.assertEqual(display.text, "£2.00", "Expected display to show £2.00 but got " + str(display))
+
+    def test_when_any_item_is_found(self):
+        display = Display()
+        point_of_sale = PointOfSale(display, {"54321": 0.99, "12345": 2.00})
+
+        point_of_sale.on_barcode("54321")
+
+        self.assertEqual(display.text, "£0.99", "Expected display to show £0.99 but got " + str(display))
+
+    def test_when_item_is_not_found(self):
+        display = Display()
+        point_of_sale = PointOfSale(display, {"11111": 1.50, "77777": 12.01})
+
+        point_of_sale.on_barcode("00000")
+
+        self.assertEqual(display.text, "Product with barcode 00000 not found")
+
+    def test_when_any_item_is_not_found(self):
+        display = Display()
+        point_of_sale = PointOfSale(display, {"54321": 1.50, "09912": 12.01})
+
+        point_of_sale.on_barcode('10011')
+
+        self.assertEqual(display.text, 'Product with barcode 10011 not found')
+
+    def test_no_barcode(self):
+        display = Display()
+        point_of_sale = PointOfSale(display, {"85439": 1.50, "83843": 12.01})
+
+        point_of_sale.on_barcode('')
+
+        self.assertEqual(display.text, 'No barcode scanned')
+
     def test_scanning_two_items_when_both_are_found(self):
         display = Display()
         point_of_sale = PointOfSale(display, {'11111': 0.99, '59385': 1.00})
