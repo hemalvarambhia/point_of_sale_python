09378592d28d2cb9a5105f05414c6c58aeb85d65
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-25 21:44:50 +0100

    Corrected a test.

--- tests/scan_multiple_items_test.py
+++ tests/scan_multiple_items_test.py
@@ -6,16 +6,13 @@ from src.point_of_sale import PointOfSale
 class ScanMultipleItems(unittest.TestCase):
     def test_scanning_two_items_when_both_are_found(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {'11111': 0.99, '59385': 1.00, '33333': 0.20})
+        point_of_sale = PointOfSale(display, {'11111': 0.99, '59385': 1.00})
 
         point_of_sale.on_barcode('11111')
         self.assertEqual(display.text, "£0.99")
 
         point_of_sale.on_barcode('59385')
         self.assertEqual(display.text, "£1.00")
-
-        point_of_sale.on_barcode('33333')
-        self.assertEqual(display.text, "£0.20")
 
     def test_scanning_two_items_but_only_one_is_found(self):
         display = Display()
