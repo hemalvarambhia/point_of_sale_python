c10baa45e293a277dba7e600151083150a61307d
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-27 22:14:35 +0100

    Added a test list for formatting monetary amounts.

---
+++ tests/formatting_monetary_amounts_test.py
@@ -0,0 +1,37 @@
+import unittest
+import pytest
+
+
+class FormattingMonetaryAmountsTest(unittest.TestCase):
+    @pytest.mark.skip(reason = 'Test list')
+    def test_amount_under_ten_pence(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_tens_of_pence(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_tens_and_units_of_pence(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_pounds_and_no_pence(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_hundreds_of_pounds_precisely(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_nine_hundred_and_ninety_nice_pounds_precisely(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_thousands_of_pounds_precisely(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
