602b3acf9f273c7ccd7ea5d98fe6a26d433d3f28
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-28 18:14:17 +0100

    Migrated clients to use display_message.

--- src/display.py
+++ src/display.py
@@ -5,17 +5,17 @@ class Display:
 
     def display_price(self, price):
         formatted_price = Display.formatted_price(price)
-        self.text = formatted_price
+        self.display_message(formatted_price)
 
     @staticmethod
     def formatted_price(price):
         return "£" + "%0.2f" % price
 
     def display_product_not_found_message(self, barcode):
-        self.text = 'Product with barcode %s not found' % barcode
+        self.display_message('Product with barcode %s not found' % barcode)
 
     def display_no_barcode_message(self):
-        self.text = 'No barcode scanned'
+        self.display_message('No barcode scanned')
 
     def display_message(self, message):
         self.text = message
