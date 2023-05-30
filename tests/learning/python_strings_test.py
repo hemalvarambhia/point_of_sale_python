76b5e579211e41a63fec92b0379ebedebc9a226c
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-30 22:27:16 +0100

    Changed the ordering to make it look like I knew what I was doing.

--- tests/learning/python_strings_test.py
+++ tests/learning/python_strings_test.py
@@ -8,12 +8,12 @@ class PythonStringsTest(unittest.TestCase):
         self.assertEqual(['1000', '00'], '1000.00'.split('.'))
         self.assertNotEqual([1000, 0], '1000.00'.split('.'))
 
-    def test_finding_all_parts_matching_a_regular_expression(self):
-        matches = re.findall(r'\d{1,3}', '0001')
-        self.assertEqual(['000', '1'], matches)
-
     def test_reversing_a_string(self):
         self.assertEqual('0001', '1000'[::-1])
+
+    def test_finding_all_parts_matching_a_regular_expression(self):
+        matches = re.findall(r'\d{1,3}', '1000'[::-1])
+        self.assertEqual(['000', '1'], matches)
 
     @pytest.mark.skip(reason = 'Test list')
     def test_joining_elements_of_an_array_into_a_string(self):
