2872c55328af6531aa2a026a28acaab4ae3b04af
hemalvarambhia <h.n.varambhia@gmail.com>
2023-05-30 22:32:05 +0100

    Removed some obsolete tests.

--- tests/learning/python_strings_test.py
+++ tests/learning/python_strings_test.py
@@ -4,10 +4,6 @@ import pytest
 
 
 class PythonStringsTest(unittest.TestCase):
-    def test_splitting_by_character(self):
-        self.assertEqual(['1000', '00'], '1000.00'.split('.'))
-        self.assertNotEqual([1000, 0], '1000.00'.split('.'))
-
     def test_reversing_a_string(self):
         self.assertEqual('0001', '1000'[::-1])
 
@@ -17,12 +13,8 @@ class PythonStringsTest(unittest.TestCase):
 
     def test_joining_strings_in_an_array(self):
         joined = ','.join(['000', '1'])[::-1]
+
         self.assertEqual('1,000', joined)
-
-    @pytest.mark.skip(reason = 'Test list')
-    def test_joining_elements_of_an_array_into_a_string(self):
-        pass
-
 
 if __name__ == '__main__':
     unittest.main()
