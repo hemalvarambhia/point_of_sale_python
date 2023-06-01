f21cd7becec9831501336740d69fa92e005b83bc
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-01 12:29:15 +0100

    Wrote another test from the list.

--- tests/formatting_monetary_amounts_test.py
+++ tests/formatting_monetary_amounts_test.py
@@ -44,9 +44,10 @@ class FormattingMonetaryAmountsTest(unittest.TestCase):
 
         self.assertEqual(formatted_price, '£1,000.99')
 
-    @pytest.mark.skip(reason='Test list')
     def test_tens_of_thousands_of_pounds_precisely(self):
-        pass
+        formatted_price = Display.formatted_price(10000.00)
+
+        self.assertEqual(formatted_price, '£10,000.00')
 
     @pytest.mark.skip(reason='Test list')
     def test_hundreds_of_thousands_of_pounds_precisely(self):
