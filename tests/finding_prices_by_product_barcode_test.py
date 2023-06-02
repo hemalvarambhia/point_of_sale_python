943353fd8120ad7aea3ed3326ecb6ff986e20f60
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-02 22:14:36 +0100

    Began writing a test list for a Product Catalogue.

---
+++ tests/finding_prices_by_product_barcode_test.py
@@ -0,0 +1,17 @@
+import unittest
+import pytest
+
+
+class FindingPricesByProductBarcodeTest(unittest.TestCase):
+
+    @pytest.mark.skip(reason='Test list')
+    def test_price_is_returned_when_barcode_is_in_catalog(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_nothing_is_returned_when_barcode_is_not_in_catalogue(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
