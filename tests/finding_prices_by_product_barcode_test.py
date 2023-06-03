922d35f96ae15f2151ede1f3137b08962bfe6503
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-03 22:47:06 +0100

    Now we can get the price of any product given its barcode.

--- tests/finding_prices_by_product_barcode_test.py
+++ tests/finding_prices_by_product_barcode_test.py
@@ -5,10 +5,10 @@ import pytest
 class Catalogue:
 
     def __init__(self, catalogue):
-        pass
+        self.catalogue = catalogue
 
     def __getitem__(self, barcode):
-        return 50
+        return self.catalogue[barcode]
 
 
 
@@ -18,6 +18,11 @@ class FindingPricesByProductBarcodeTest(unittest.TestCase):
         catalog = Catalogue({'12345': 50, '54321': 25, '11111': 2})
 
         self.assertEqual(catalog['12345'], 50)
+
+    def test_price_is_return_for_any_barcode_in_catalogue(self):
+        catalog = Catalogue({'12345': 50, '55555': 12})
+
+        self.assertEqual(catalog['55555'], 12)
 
     @pytest.mark.skip(reason='Test list')
     def test_nothing_is_returned_when_barcode_is_not_in_catalogue(self):
