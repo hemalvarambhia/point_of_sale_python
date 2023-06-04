ef45df878b6e67b7837c4f3d9896ccd9e5204357
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-04 20:14:15 +0100

    Migrated the tests to use the new Catalogue object.

--- tests/selling_items_test.py
+++ tests/selling_items_test.py
@@ -1,12 +1,14 @@
 import unittest
 from src.display import Display
 from src.point_of_sale import PointOfSale
+from src.catalogue import Catalogue
 
 
 class SellingItemsTest(unittest.TestCase):
     def test_selling_one_item(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {"54321": 0.99, "12345": 3.99})
+        catalogue = Catalogue({"54321": 0.99, "12345": 3.99})
+        point_of_sale = PointOfSale(display, catalogue)
         point_of_sale.on_barcode("12345")
 
         point_of_sale.on_total()
@@ -15,7 +17,8 @@ class SellingItemsTest(unittest.TestCase):
 
     def test_selling_one_item_and_product_is_not_found(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {"93457": 0.99, "58043": 3.99})
+        catalogue = Catalogue({"93457": 0.99, "58043": 3.99})
+        point_of_sale = PointOfSale(display, catalogue)
         point_of_sale.on_barcode("99999")
 
         point_of_sale.on_total()
@@ -24,7 +27,8 @@ class SellingItemsTest(unittest.TestCase):
 
     def test_selling_two_items_when_all_are_found(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {"32323": 10.99, "84583": 2.00})
+        catalogue = Catalogue({"32323": 10.99, "84583": 2.00})
+        point_of_sale = PointOfSale(display, catalogue)
         point_of_sale.on_barcode("32323")
         point_of_sale.on_barcode("84583")
 
@@ -34,7 +38,8 @@ class SellingItemsTest(unittest.TestCase):
 
     def test_scanning_two_items_items_when_only_one_is_found(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {"89999": 108.01})
+        catalogue = Catalogue({"89999": 108.01})
+        point_of_sale = PointOfSale(display, catalogue)
         point_of_sale.on_barcode("44444")
         point_of_sale.on_barcode("89999")
 
@@ -44,7 +49,8 @@ class SellingItemsTest(unittest.TestCase):
 
     def test_selling_three_items_when_none_are_found(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {"09876": 108.01})
+        catalogue = Catalogue({"09876": 108.01})
+        point_of_sale = PointOfSale(display, catalogue)
         point_of_sale.on_barcode("00000")
         point_of_sale.on_barcode("11111")
         point_of_sale.on_barcode("22222")
@@ -55,7 +61,8 @@ class SellingItemsTest(unittest.TestCase):
 
     def test_selling_three_items_when_all_are_found(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {"09911": 100.01, "21199": 51.75, "31394": 100.02})
+        catalogue = Catalogue({"09911": 100.01, "21199": 51.75, "31394": 100.02})
+        point_of_sale = PointOfSale(display, catalogue)
         point_of_sale.on_barcode("09911")
         point_of_sale.on_barcode("21199")
         point_of_sale.on_barcode("31394")
@@ -66,7 +73,8 @@ class SellingItemsTest(unittest.TestCase):
 
     def test_selling_items_totalling_thousands_of_currency_units(self):
         display = Display()
-        point_of_sale = PointOfSale(display, {"54321": 500.01, "98765": 510.75, "00001": 100.25})
+        catalogue = Catalogue({"54321": 500.01, "98765": 510.75, "00001": 100.25})
+        point_of_sale = PointOfSale(display, catalogue)
         point_of_sale.on_barcode("98765")
         point_of_sale.on_barcode("00001")
         point_of_sale.on_barcode("54321")
