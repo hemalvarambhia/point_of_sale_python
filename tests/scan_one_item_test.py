649dbee9fdf47b1a02342091a85b9caf4b313cc5
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-13 22:16:52 +0100

    Corrected the code style.

--- tests/scan_one_item_test.py
+++ tests/scan_one_item_test.py
@@ -5,11 +5,11 @@ class ScanOneItemTest(unittest.TestCase):
     def test_failing_hookup(self):
         self.assertEqual(1 + 1, 2)  # add assertion here
 
-    @pytest.mark.skip(reason = 'Test list')
+    @pytest.mark.skip(reason='Test list')
     def test_scanning_one_item_that_is_in_catalogue(self):
         pass
 
-    @pytest.mark.skip(reason = 'Test list')
+    @pytest.mark.skip(reason='Test list')
     def test_scanning_one_item_that_is_not_in_catalogue(self):
         pass
 
