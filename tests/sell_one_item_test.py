259cfa50665a5103ff7a436a64659d832c789261
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-13 22:09:12 +0100

    Tests passed

--- tests/sell_one_item_test.py
+++ tests/sell_one_item_test.py
@@ -3,7 +3,7 @@ import unittest
 
 class SellOneItemTest(unittest.TestCase):
     def test_failing_hookup(self):
-        self.assertEqual(1 + 1, 3)  # add assertion here
+        self.assertEqual(1 + 1, 2)  # add assertion here
 
 
 if __name__ == '__main__':
