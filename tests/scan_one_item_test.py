80cb7aae867268226e3844d0ef56c941c4cf2443
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-13 22:16:18 +0100

    Added tests to the list.

--- tests/scan_one_item_test.py
+++ tests/scan_one_item_test.py
@@ -1,9 +1,17 @@
 import unittest
-
+import pytest
 
 class ScanOneItemTest(unittest.TestCase):
     def test_failing_hookup(self):
         self.assertEqual(1 + 1, 2)  # add assertion here
+
+    @pytest.mark.skip(reason = 'Test list')
+    def test_scanning_one_item_that_is_in_catalogue(self):
+        pass
+
+    @pytest.mark.skip(reason = 'Test list')
+    def test_scanning_one_item_that_is_not_in_catalogue(self):
+        pass
 
 
 if __name__ == '__main__':
