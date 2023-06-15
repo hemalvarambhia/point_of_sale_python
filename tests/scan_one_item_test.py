1053d07737f09c70fe8a64f499fac5077ef7eff9
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-15 18:10:56 +0100

    We can display the price of any item as long as it is £1.99.

--- tests/scan_one_item_test.py
+++ tests/scan_one_item_test.py
@@ -1,14 +1,27 @@
 import unittest
+from unittest.mock import Mock
 import pytest
+
+
+class PointOfSale:
+    def __init__(self, display):
+        self.display = display
+
+    def on_barcode(self, barcode):
+        self.display.display_price('£1.99')
 
 
 class ScanOneItemTest(unittest.TestCase):
     def test_failing_hookup(self):
         self.assertEqual(1 + 1, 2)  # add assertion here
 
-    @pytest.mark.skip(reason='Test list')
     def test_scanning_one_item_that_is_in_catalogue(self):
-        pass
+        display = Mock()
+
+        point_of_sale_terminal = PointOfSale(display)
+        point_of_sale_terminal.on_barcode('12345')
+
+        display.display_price.assert_called_with('£1.99')
 
     @pytest.mark.skip(reason='Test list')
     def test_scanning_one_item_that_is_not_in_catalogue(self):
