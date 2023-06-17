50bdeaeebea3ad6c5c8489386a3297485dd2e052
hemalvarambhia <h.n.varambhia@gmail.com>
2023-06-17 17:11:10 +0100

    When the product is not in the catalogue, we display a product not found message.

--- tests/scan_one_item_test.py
+++ tests/scan_one_item_test.py
@@ -10,8 +10,11 @@ class PointOfSale:
 
     def on_barcode(self, barcode):
         price = self.catalogue.price_for_barcode('12345')
-        text = '£%.2f' % (price / 100)
-        self.display.display_price(text)
+        if price is None:
+            self.display.display_product_not_found(barcode)
+        else:
+            text = '£%.2f' % (price / 100)
+            self.display.display_price(text)
 
 
 class ScanOneItemTest(unittest.TestCase):
@@ -25,9 +28,14 @@ class ScanOneItemTest(unittest.TestCase):
 
         display.display_price.assert_called_with('£1.99')
 
-    @pytest.mark.skip(reason='Test list')
     def test_scanning_one_item_that_is_not_in_catalogue(self):
-        pass
+        display = Mock()
+        catalogue = Mock()
+        catalogue.configure_mock(**{'price_for_barcode.return_value': None})
+        point_of_sale_terminal = PointOfSale(catalogue, display)
+        point_of_sale_terminal.on_barcode('56344')
+
+        display.display_product_not_found.assert_called_with('56344')
 
 
 if __name__ == '__main__':
