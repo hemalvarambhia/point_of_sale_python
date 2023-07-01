7dc06aa19522e381998bc60926d208a4f03d9363
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-01 23:19:18 +0100

    Produced an ongoing test list for formatting monetary amounts in the western style.

---
+++ tests/formatting_monetary_amounts_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+import pytest
+
+
+class FormattingMonetaryAmountsTest(unittest.TestCase):
+
+    @pytest.mark.skip(reason='Test list')
+    def test_formatting_amounts_under_ten_pence(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_formatting_amounts_between_nine_pence_and_under_1_pound(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_formatting_one_pound(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_formatting_amounts_between_one_pound_and_under_one_thousand_pounds(self):
+        pass
