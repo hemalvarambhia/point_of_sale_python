f5fe3ab95824bca2760a10e76b401d0840865d2e
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-02 23:29:16 +0100

    Improved the names of some tests.

--- tests/python_re_learning_test.py
+++ tests/python_re_learning_test.py
@@ -4,14 +4,15 @@ import unittest
 
 class PythonReLearningTest(unittest.TestCase):
 
-    def test_splitting_thousands_of_currency_after_three_zeros(self):
+    def test_splitting_thousands_of_currency_units_after_three_zeros(self):
         regular_expression = re.compile(r'\d{1,3}')
         self.assertEqual(['000', '1'], regular_expression.findall('0001'))
 
     def test_reversing_a_string(self):
         self.assertEqual('0001', ''.join(reversed('1000')))
 
-    def test_decomposing_a_monetary_amount_into_pounds_and_pence(self):
+    # units => pounds/dollars, subunits => pence/cents
+    def test_decomposing_a_monetary_amount_into_units_and_subunits(self):
         monetary_amount = '1000.00'
         index_of_decimal_point = monetary_amount.index('.')
         pounds = monetary_amount[:index_of_decimal_point]
