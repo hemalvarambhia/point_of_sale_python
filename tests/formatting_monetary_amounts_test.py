2d519501167eb441906ce626cd0987dc9118e040
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-02 18:07:01 +0100

    Added more tests from the list.

--- tests/formatting_monetary_amounts_test.py
+++ tests/formatting_monetary_amounts_test.py
@@ -8,17 +8,14 @@ class FormattingMonetaryAmountsTest(unittest.TestCase):
     def test_formatting_amounts_under_ten_pence(self):
         self.assertEqual("£0.09", self.format_monetary_amount(9))
 
-    @pytest.mark.skip(reason='Test list')
     def test_formatting_amounts_between_nine_pence_and_under_1_pound(self):
-        pass
+        self.assertEqual("£0.19", self.format_monetary_amount(19))
 
-    @pytest.mark.skip(reason='Test list')
     def test_formatting_exactly_one_pound(self):
-        pass
+        self.assertEqual("£1.00", self.format_monetary_amount(100))
 
-    @pytest.mark.skip(reason='Test list')
     def test_formatting_amounts_between_one_pound_and_under_one_thousand_pounds(self):
-        pass
+        self.assertEqual("£999.00", self.format_monetary_amount(99900))
 
     @pytest.mark.skip(reason='Test list')
     def test_formatting_exactly_one_thousand_pounds(self):
