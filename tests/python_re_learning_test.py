bc91d20b79d6ea3010f781225ead26134367f945
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-02 23:17:29 +0100

    Wrote a learning test that documents how to reverse a string.

--- tests/python_re_learning_test.py
+++ tests/python_re_learning_test.py
@@ -7,3 +7,6 @@ class PythonReLearningTest(unittest.TestCase):
     def test_splitting_thousands_after_three_zeros(self):
         regular_expression = re.compile(r'\d{1,3}')
         self.assertEqual(['000', '1'], regular_expression.findall('0001'))
+
+    def test_reversing_a_string(self):
+        self.assertEqual('0001', ''.join(reversed('1000')))
