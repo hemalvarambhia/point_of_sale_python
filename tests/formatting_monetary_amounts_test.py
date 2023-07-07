ff5a654876f89d252e804925bb5f2609dd0bab99
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-07 23:03:03 +0100

    Made similar code even more similar.

--- tests/formatting_monetary_amounts_test.py
+++ tests/formatting_monetary_amounts_test.py
@@ -15,7 +15,14 @@ def format_monetary_amount(amount_in_pence):
         subunits = amount_as_text[index_of_decimal_point + 1:]
         return '£%s.%s' % (units, subunits)
     else:
-        return '£%.2f' % in_pounds
+        amount_as_text = '%.2f' % in_pounds
+        index_of_decimal_point = amount_as_text.index('.')
+        units = amount_as_text[:index_of_decimal_point]
+        thousands_matcher = re.compile(r'\d{1,3}')
+        decomposed = thousands_matcher.findall(''.join(reversed(units)))
+        units = ''.join(reversed(','.join(decomposed)))
+        subunits = amount_as_text[index_of_decimal_point + 1:]
+        return '£%s.%s' % (units, subunits)
 
 
 class FormattingMonetaryAmountsTest(unittest.TestCase):
