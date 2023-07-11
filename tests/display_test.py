c9ac90f2090295f994911bd5fff793c6e4f0e0fa
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-11 23:02:45 +0100

    We can now format pounds and pence.

--- tests/display_test.py
+++ tests/display_test.py
@@ -7,7 +7,7 @@ class Display:
         self.text = ''
 
     def display_price(self, price_in_pence):
-        self.text = '£0.05'
+        self.text = '£%.2f' % (price_in_pence / 100)
 
 class SellingMultipleItemsTest(unittest.TestCase):
 
@@ -18,9 +18,12 @@ class SellingMultipleItemsTest(unittest.TestCase):
 
         self.assertEqual('£0.05', display.text)
 
-    @pytest.mark.skip(reason='Test list')
     def test_displaying_a_price_that_is_pounds_only(self):
-        pass
+        display = Display()
+
+        display.display_price(1200)
+
+        self.assertEqual('£12.00', display.text)
 
     @pytest.mark.skip(reason='Test list')
     def test_displaying_a_price_that_is_pounds_and_pence(self):
