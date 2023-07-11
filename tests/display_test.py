3bbaffa24a857c751fb66519eee082f341923e44
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-11 22:12:10 +0100

    Added a test list for displaying prices.

---
+++ tests/display_test.py
@@ -0,0 +1,29 @@
+import unittest
+import pytest
+
+
+class SellingMultipleItemsTest(unittest.TestCase):
+
+    @pytest.mark.skip(reason='Test list')
+    def test_displaying_a_price_that_is_pence_only(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_displaying_a_price_that_is_pounds_only(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_displaying_a_price_that_is_pounds_and_pence(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_displaying_a_price_that_is_thousands_pounds_exactly(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_displaying_a_price_that_is_hundreds_of_thousands_pounds_exactly(self):
+        pass
+
+    @pytest.mark.skip(reason='Test list')
+    def test_displaying_a_price_that_is_millions_of_pounds_exactly(self):
+        pass
