ccc818b1ffcf9a6060a60b1a50b86d3b5b99a3eb
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-14 22:35:35 +0100

    Removed all tests in favour of data driven testing.

--- tests/display_test.py
+++ tests/display_test.py
@@ -13,45 +13,6 @@ class Display:
 
 class DisplayTest(unittest.TestCase):
 
-    def test_displaying_a_price_that_is_pence_only(self):
-        display = Display()
-
-        display.display_price(5)
-
-        self.assertEqual('£0.05', display.text)
-
-    def test_displaying_a_price_that_is_pounds_only(self):
-        display = Display()
-
-        display.display_price(1200)
-
-        self.assertEqual('£12.00', display.text)
-
-    @pytest.mark.skip(reason='Test list')
-    def test_displaying_a_price_that_is_pounds_and_pence(self):
-        pass
-
-    def test_displaying_a_price_that_is_thousands_pounds_exactly(self):
-        display = Display()
-
-        display.display_price(100000)
-
-        self.assertEqual('£1,000.00', display.text)
-
-    def test_displaying_a_price_that_is_hundreds_of_thousands_pounds_exactly(self):
-        display = Display()
-
-        display.display_price(10000000)
-
-        self.assertEqual('£100,000.00', display.text)
-
-    def test_displaying_a_price_that_is_millions_of_pounds_exactly(self):
-        display = Display()
-
-        display.display_price(100000000)
-
-        self.assertEqual('£1,000,000.00', display.text)
-
     def test_displaying_monetary_amounts_formatted_in_the_western_system(self):
         amounts_and_formats = {
             5: '£0.05',
