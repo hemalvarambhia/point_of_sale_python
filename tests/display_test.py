3d3829b4a87a8a79dc3779fcb7fb9b2b55982caa
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-14 22:38:58 +0100

    We now use the class in the production code.

--- tests/display_test.py
+++ tests/display_test.py
@@ -1,14 +1,5 @@
 import unittest
-import pytest
-from src.monetary_amount_formatter import MonetaryAmountFormatter
-
-
-class Display:
-    def __init__(self):
-        self.text = ''
-
-    def display_price(self, price_in_pence):
-        self.text = MonetaryAmountFormatter.format_monetary_amount(price_in_pence)
+from src.display import Display
 
 
 class DisplayTest(unittest.TestCase):
