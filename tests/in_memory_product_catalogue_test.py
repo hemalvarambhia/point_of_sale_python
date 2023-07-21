06a7295cbb9eb53e4858fb7c0d310b6980d07e68
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-21 23:16:29 +0100

    Use unittest's skip feature.

--- tests/in_memory_product_catalogue_test.py
+++ tests/in_memory_product_catalogue_test.py
@@ -1,13 +1,12 @@
 import unittest
-import pytest
 
 
 class InMemoryProductCatalogueTest(unittest.TestCase):
 
-    @pytest.mark.skip(reason='Test list')
+    @unittest.skip('Test list')
     def test_product_listed_in_catalogue(self):
         pass
 
-    @pytest.mark.skip(reason='Test list')
+    @unittest.skip('Test list')
     def test_product_missing_from_catalogue(self):
         pass
