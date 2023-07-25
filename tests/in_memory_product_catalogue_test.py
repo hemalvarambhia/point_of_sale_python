88a9ff1242d8af63b414202f8c5b2a4b14cc1036
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-25 20:19:20 +0100

    Fake it til we make it - we can return the price of any barcode so long as it's 199p.

--- tests/in_memory_product_catalogue_test.py
+++ tests/in_memory_product_catalogue_test.py
@@ -1,11 +1,12 @@
 import unittest
+from src.catalogue import Catalogue
 
 
 class InMemoryProductCatalogueTest(unittest.TestCase):
 
-    @unittest.skip('Test list')
     def test_finding_price_of_product_listed_in_catalogue(self):
-        pass
+        catalogue = Catalogue({12345: 199})
+        self.assertEqual(199, catalogue.price_for_barcode('12345'))
 
     @unittest.skip('Test list')
     def test_finding_no_price_when_product_is_not_in_catalogue(self):
