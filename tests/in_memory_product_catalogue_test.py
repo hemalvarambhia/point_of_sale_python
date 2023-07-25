8c4ae68c7d4406df30e2646bee0591ef107141bd
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-25 20:13:35 +0100

    Clarified the names of the tests.

--- tests/in_memory_product_catalogue_test.py
+++ tests/in_memory_product_catalogue_test.py
@@ -4,9 +4,9 @@ import unittest
 class InMemoryProductCatalogueTest(unittest.TestCase):
 
     @unittest.skip('Test list')
-    def test_product_listed_in_catalogue(self):
+    def test_finding_price_of_product_listed_in_catalogue(self):
         pass
 
     @unittest.skip('Test list')
-    def test_product_missing_from_catalogue(self):
+    def test_finding_no_price_when_product_is_not_in_catalogue(self):
         pass
