897e111248d5788b932771d16a217fcad940963a
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-25 23:22:22 +0100

    The currency is set from the constructor argument.

--- src/monetary_amount_formatter.py
+++ src/monetary_amount_formatter.py
@@ -7,7 +7,7 @@ class MonetaryAmountFormatter:
         return MonetaryAmountFormatter().format(amount_in_pence)
 
     def __init__(self, currency='£'):
-        self.currency = '£'
+        self.currency = currency
 
     def format(self, amount_in_subunits):
         in_currency_units = amount_in_subunits / 100
