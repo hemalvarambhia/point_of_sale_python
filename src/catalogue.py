7929a9275989bee981eb7cfc03846ba8aeaf337a
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-25 20:20:37 +0100

    The barcode is really a string.

---
+++ src/catalogue.py
@@ -0,0 +1,6 @@
+class Catalogue:
+    def __init__(self, prices_by_barcode):
+        pass
+
+    def price_for_barcode(self, barcode):
+        return 199
--- tests/in_memory_product_catalogue_test.py
+++ tests/in_memory_product_catalogue_test.py
@@ -5,7 +5,7 @@ from src.catalogue import Catalogue
 class InMemoryProductCatalogueTest(unittest.TestCase):
 
     def test_finding_price_of_product_listed_in_catalogue(self):
-        catalogue = Catalogue({12345: 199})
+        catalogue = Catalogue({'12345': 199})
         self.assertEqual(199, catalogue.price_for_barcode('12345'))
 
     @unittest.skip('Test list')
