d31eb193d6b8599918b5321b89fbd908169170cc
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-25 20:29:04 +0100

    Inlined the method as it did very little.

--- tests/formatting_monetary_amounts_test.py
+++ tests/formatting_monetary_amounts_test.py
@@ -2,29 +2,25 @@ import unittest
 from src.monetary_amount_formatter import MonetaryAmountFormatter
 
 
-def format_monetary_amount(amount_in_pence):
-    return MonetaryAmountFormatter.format_monetary_amount(amount_in_pence)
-
-
 class FormattingMonetaryAmountsTest(unittest.TestCase):
 
     def test_formatting_amounts_under_ten_pence(self):
-        self.assertEqual("£0.09", format_monetary_amount(9))
+        self.assertEqual("£0.09", MonetaryAmountFormatter.format_monetary_amount(9))
 
     def test_formatting_amounts_between_nine_pence_and_under_1_pound(self):
-        self.assertEqual("£0.19", format_monetary_amount(19))
+        self.assertEqual("£0.19", MonetaryAmountFormatter.format_monetary_amount(19))
 
     def test_formatting_exactly_one_pound(self):
-        self.assertEqual("£1.00", format_monetary_amount(100))
+        self.assertEqual("£1.00", MonetaryAmountFormatter.format_monetary_amount(100))
 
     def test_formatting_amounts_between_one_pound_and_under_one_thousand_pounds(self):
-        self.assertEqual("£999.00", format_monetary_amount(99900))
+        self.assertEqual("£999.00", MonetaryAmountFormatter.format_monetary_amount(99900))
 
     def test_formatting_exactly_one_thousand_pounds(self):
-        self.assertEqual("£1,000.00", format_monetary_amount(100000))
+        self.assertEqual("£1,000.00", MonetaryAmountFormatter.format_monetary_amount(100000))
 
     def test_formatting_thousand_pounds_and_some_pence(self):
-        self.assertEqual("£1,000.99", format_monetary_amount(100099))
+        self.assertEqual("£1,000.99", MonetaryAmountFormatter.format_monetary_amount(100099))
 
     def test_formatting_tens_of_thousands_pounds_exactly(self):
-        self.assertEqual("£10,000.00", format_monetary_amount(1000000))
+        self.assertEqual("£10,000.00", MonetaryAmountFormatter.format_monetary_amount(1000000))
