318991675368d951aaf539fa40f608158b7296ea
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-25 23:13:33 +0100

    Migrated away to using instance methods.

--- src/monetary_amount_formatter.py
+++ src/monetary_amount_formatter.py
@@ -4,6 +4,9 @@ import re
 class MonetaryAmountFormatter:
     @staticmethod
     def format_monetary_amount(amount_in_pence):
+        return MonetaryAmountFormatter().format(amount_in_pence)
+
+    def format(self, amount_in_pence):
         in_pounds = amount_in_pence / 100
         amount_as_text = '%.2f' % in_pounds
         index_of_decimal_point = amount_as_text.index('.')
